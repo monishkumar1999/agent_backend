0dda2a8a6b65714d754d8d3db926ea6a9af62cdf
Athiban7 <athiban2019.com>
2025-02-20 15:19:55 +0530

    2d

--- route/users/userRoute.js
+++ route/users/userRoute.js
@@ -1,19 +1,32 @@
 const express = require("express");
-const { addUser,loginUser, loginWithGoogle,updateUserProfile,upload,verifyOtp } = require("../../controller/userController");
+const {
+  addUser,
+  loginUser,
+  loginWithGoogle,
+  updateUserProfile,
+  upload,
+  verifyOtp,
+} = require("../../controller/userController");
 const verifyUserJwt = require("../../middleware/verifyUserJwt");
 const multer = require("multer");
-const { addProposal, getAgentsByProposal } = require("../../controller/ProposalController");
+const {
+  addProposal,
+  getAgentsByProposal,
+} = require("../../controller/ProposalController");
 
 const userRouter = express.Router();
 userRouter.post("/add", addUser);
 userRouter.post("/login", loginUser);
-userRouter.post("/google-login",loginWithGoogle);
-userRouter.put("/userProfile-update", verifyUserJwt, upload.single("profileImage"), updateUserProfile);
-userRouter.get("/verify-otp", verifyOtp); 
-userRouter.post("/store-proposal",verifyUserJwt,addProposal)
+userRouter.post("/google-login", loginWithGoogle);
+userRouter.put(
+  "/userProfile-update",
+  verifyUserJwt,
+  upload.single("profileImage"),
+  updateUserProfile
+);
+userRouter.post("/verify-otp", verifyOtp);
+userRouter.post("/store-proposal", verifyUserJwt, addProposal);
 
-userRouter.post("/find-agent",verifyUserJwt,getAgentsByProposal )
-
-
+userRouter.post("/find-agent", verifyUserJwt, getAgentsByProposal);
 
 module.exports = userRouter;
